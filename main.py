201c560ba3ffc733dba917db86ae82cba5569aa0
hugob1234 <159055356+hugob1234@users.noreply.github.com>
2024-02-13 20:25:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 import string
-x = int(input("Enter length of password: "))
-def password(length=x):
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(x))
 
-gen = password(x)
-print(f"Your new password is : {gen}")
+while True:
+    x = int(input("Enter length of password: "))
+    def password(length=x):
+        characters = string.ascii_letters + string.digits
+        return ''.join(random.choice(characters) for _ in range(x))
+
+    gen = password(x)
+    print(f"Your new password is : {gen}")
     
